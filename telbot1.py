ec010c1de78917461df8d235c9dd9d81a0b0513c
piv <pivwork001@mail.ru>
2016-12-03 18:42:32 +0300

    first handler

--- telbot1.py
+++ telbot1.py
@@ -1,8 +1,13 @@
 #import python_telegram_bot
-from telegram.ext import Updater
+from telegram.ext import Updater, CommandHandler
+
+def greet_user(bot, update):
+    print('Вызван /start')
 
 def main():
 	updater = Updater('312255597:AAHeQ-xtkTqSUMpNW08TXhFPLeaQtQGE5D4')
+	dp = updater.dispatcher
+	dp.add_handler(CommandHandler("start", greet_user))
 	updater.start_polling()
 	updater.idle()
 
