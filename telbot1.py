24fc395779eb630795050e921581e44eed6bf0b8
piv <pivwork001@mail.ru>
2016-12-03 19:08:49 +0300

    echo

--- telbot1.py
+++ telbot1.py
@@ -1,5 +1,9 @@
 #import python_telegram_bot
-from telegram.ext import Updater, CommandHandler
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+def talk_to_me( bot, update ):
+	print('Пришло сообщение:{}'.format(update.message.text))
+	bot.sendMessage(update.message.chat_id,'ты сказал: ' + update.message.text)
 
 def show_error(bot, update, error):
 	print('update "{}" error "{}"'.format(update,error))
@@ -15,6 +19,7 @@ def main():
 	updater = Updater('312255597:AAHeQ-xtkTqSUMpNW08TXhFPLeaQtQGE5D4')
 	dp = updater.dispatcher
 	dp.add_handler(CommandHandler("start", greet_user))
+	dp.add_handler(MessageHandler([Filters.text], talk_to_me))
 	dp.add_error_handler(show_error)
 	updater.start_polling()
 	updater.idle()
