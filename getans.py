71aaa81ac1449c01e616fa9a3f5e60c6600a0f52
piv <pivwork001@mail.ru>
2016-12-06 20:22:18 +0300

    задание по while

---
+++ getans.py
@@ -0,0 +1,22 @@
+def get_bot_dict():
+    return {'привет':'И тебе привет!','как дела': 'лучше всех', 'пока':'увидимся'}
+
+def make_ans( d, s, defans ) :
+    return d.get( s, defans )
+
+def get_ans( str ) :
+    str = str.lower()
+    return make_ans( get_bot_dict(), str, 'даже не знаю' )
+
+def interact_ans() :
+    r = ''
+    while True :
+        r = input(':')
+        print(get_ans( r ) )
+        if r == 'пока':
+            return
+
+           
+if __name__ == '__main__':
+    interact_ans() 
+
---
+++ whiletask.py
@@ -0,0 +1,55 @@
+import getans
+from getans import get_ans
+
+def cmd_help():
+	print('доступные команды:')
+	print('\tq - выход')
+	print('\t1 - поиск имени в списке')
+	print('\t2 - как дела')
+	print('\t3 - бот')
+
+
+def find_person( l, name ):
+	while len(l):
+		print('проверяем ', l[-1] )
+		if l[-1] == name:
+			print(name,' нашелся')
+			return
+		l.pop()
+	print(name,' не найден')
+
+def cmd_findname():
+	l = ["Вася", "Маша", "Петя", "Валера", "Саша", "Даша"]
+	name = 'Валера'
+	print( 'Ищем ',name ,' в списке:', l ) 
+	find_person( l, name )
+
+def ask_user():
+	ans = ''
+	while not ans == 'Хорошо':
+		ans = input('Как дела? ')
+	print('так держать')
+
+def cmd_bot():
+	ans = ''
+	while not ans == 'Пока!':
+		ans = input('Как дела?')
+		print(get_ans( ans ) )
+
+def main():
+	cmd_help()
+	cmd = ''
+	while not cmd == 'q':
+		cmd = input('введите команду (q)')
+		if cmd == '1':
+			cmd_findname()			
+		elif cmd == '2':
+			ask_user()
+		elif cmd == '3':
+			cmd_bot()
+		else :
+			cmd_help()
+
+
+if __name__ == '__main__':
+    main() 
