48449ec87dedde1cc4f8c809266fc40c5ac189d9
piv <pivwork001@mail.ru>
2016-12-10 16:39:30 +0300

    команда подсчет слов

--- telbot1.py
+++ telbot1.py
@@ -21,12 +21,48 @@ def greet_user(bot, update):
     print(type(bot))
     print(type(update))
     print(update.message)
-    bot.sendMessage(update.message.chat_id, text='Скажи что нибудь')
+    command_info = '''
+    Доступны следующие команды:
+    /start - эта информация
+    /wordcount [слова в кавычках разделенные пробелом] - подсчитает количество введенных слов
+    также можно просто что нибудь написать и тогда я что то отвечу
+    '''
+    bot.sendMessage(update.message.chat_id, text=command_info)
+
+
+def word_counter(msg_text):
+    print('word_counter', msg_text)
+    begin_index = msg_text.find('"')
+    if begin_index == -1:
+        return 'Надо было ввести слова в кавычках'
+
+    end_index = msg_text.rfind('"')
+    if begin_index == end_index or begin_index + 1 == end_index:
+        return 'Надо было ввести слова в кавычках'
+
+    user_words = msg_text[begin_index + 1:end_index].strip()
+    user_words = user_words.strip()
+    splited = user_words.split(' ')
+    w_count = 0
+    for i in splited:
+        if i != ' ' and i != '':
+            w_count += 1
+    if not w_count:
+        return 'в кавычках должны были быть слова'
+    return 'Количество введенных слов {}'.format(w_count)
+
+def word_count(bot, update):
+    '''Считаем количество слов переданных пользователем.'''
+    print('Вызван /wordcount')
+    print(type(bot))
+    print(type(update))
+    bot.sendMessage(update.message.chat_id, text=word_counter(update.message.text))
 
 def main():
     '''Главная функция.'''
     updater = Updater('312255597:AAHeQ-xtkTqSUMpNW08TXhFPLeaQtQGE5D4')
     updater.dispatcher.add_handler(CommandHandler("start", greet_user))
+    updater.dispatcher.add_handler(CommandHandler("wordcount", word_count))
     updater.dispatcher.add_handler(MessageHandler([Filters.text], talk_to_me))
     updater.dispatcher.add_error_handler(show_error)
     updater.start_polling()
@@ -35,3 +71,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+#    print(word_counter(input('введите текст в кавычках разделенный пробелами: ')))
