e80dca2257c765d1eb6f145d4422b086ca2fb6b7
piv <pivwork001@mail.ru>
2016-12-14 21:25:11 +0300

    'calc_from_words_message'

--- telbot1.py
+++ telbot1.py
@@ -1,6 +1,7 @@
 '''Простой бот для telegram.'''
 #import python_telegram_bot
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram import ReplyKeyboardMarkup
 from getans import get_ans, rand_positive
 import re
 
@@ -22,33 +23,71 @@ def calc_from_message(msg_text):
     msg_text = ' ' + msg_text.strip()
     end_index = msg_text.rfind('=')
     if end_index == -1:
-        return 'Введите выражение вида 1+2='
+        msg_text += '='
 
-    match_result = re.match('(\D*)(\d*)([+,*,/,-])(\d*)=', msg_text)
+    match_result = re.match('(\D*)([\d.]*)([+,*,/,-])([\d.]*)(\D*)', msg_text)
     if not match_result:
         return 'Введите выражение вида 1+2='
 
     exp_result = None
     try:
         if match_result.group(3) == '+':
-            exp_result = int(match_result.group(2)) + int(match_result.group(4))
+            exp_result = float(match_result.group(2)) + float(match_result.group(4))
         elif match_result.group(3) == '*':
-            exp_result = int(match_result.group(2)) * int(match_result.group(4))
+            exp_result = float(match_result.group(2)) * float(match_result.group(4))
         elif match_result.group(3) == '/':
             if not int(match_result.group(4)):
                 return 'деление на ноль не поддерживается'
-            exp_result = int(match_result.group(2)) / int(match_result.group(4))
+            exp_result = float(match_result.group(2)) / float(match_result.group(4))
         elif match_result.group(3) == '-':
-            exp_result = int(match_result.group(2)) - int(match_result.group(4))
+            exp_result = float(match_result.group(2)) - float(match_result.group(4))
         else:
             return 'Ошибка программиста сюда попасть не должны'
     except TypeError:
         return 'Неподдерживаемый тип'
 
     if exp_result:
+        if int(exp_result) != exp_result:
+            exp_result = round(exp_result, 2)
         return 'Результат подсчета:{}'.format(exp_result)
 
     return 'Не удалось вычислить результат выражения [{}]'.format(msg_text)
+
+def calc_from_words_message(msg_text):
+    '''Пытаемся обработать строку которая может содержать слова вместо цифр.
+    [один плюс два]
+    '''
+    msg_text = msg_text.lower()
+    msg_text = msg_text.replace('сколько будет','')
+    msg_text = msg_text.replace('один','1')
+    msg_text = msg_text.replace('два','2')
+    msg_text = msg_text.replace('три','3')
+    msg_text = msg_text.replace('четыре','4')
+    msg_text = msg_text.replace('пять','5')
+    msg_text = msg_text.replace('шесть','6')
+    msg_text = msg_text.replace('семь','7')
+    msg_text = msg_text.replace('восемь','8')
+    msg_text = msg_text.replace('девять','9')
+    msg_text = msg_text.replace('десять','10')
+    msg_text = msg_text.replace('плюс','+')
+    msg_text = msg_text.replace('прибавить','+')
+    msg_text = msg_text.replace('минус','-')
+    msg_text = msg_text.replace('вычесть','-')
+    msg_text = msg_text.replace('отнять','-')
+    msg_text = msg_text.replace('умножить на','*')
+    msg_text = msg_text.replace('умножить','*')
+    msg_text = msg_text.replace('делить на','/')
+    msg_text = msg_text.replace('делить','/')
+    msg_text = msg_text.replace(':','/')
+    msg_text = msg_text.replace('разделить','/')
+    msg_text = msg_text.replace('разделить на','/')
+    msg_text = msg_text.replace('равно','=')
+    msg_text = msg_text.replace('равняется','=')
+    msg_text = msg_text.replace(',','.')
+    msg_text = msg_text.replace(' и ','.')
+    msg_text = msg_text.replace(' ','')
+    return calc_from_message(msg_text)
+
 
 def word_counter(msg_text):
     '''Считаем количество слов.'''
@@ -85,7 +124,10 @@ def cmd_calc(bot, update):
     print(type(bot))
     print(type(update))
     print(update.message)
-    bot.sendMessage(update.message.chat_id, text=calc_from_message(update.message.text))
+    calc_keyboard = [['0','1','2','3','4'],['5','6','7','8','9'], ['+','-','*','/','=']]
+    reply_markup = ReplyKeyboardMarkup(calc_keyboard)
+    bot.sendMessage(update.message.chat_id, text='Калькулятор', reply_markup=reply_markup)    
+    bot.sendMessage(update.message.chat_id, text=calc_from_words_message(update.message.text))
 
 def greet_user(bot, update):
     '''Приветствие пользователя по команде /start.'''
@@ -115,6 +157,7 @@ def main():
     updater.idle()
 
 if __name__ == '__main__':
-    main()
+   main()
 #    print(word_counter(input('введите текст в кавычках разделенный пробелами: ')))
 #    print(calc_from_message(input('введите выражение вида 1+2=: ')))
+#    print(calc_from_words_message(input('введите выражение вида 1+2=: ')))
