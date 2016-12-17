08ccfbb56824fce25f2b449b6ab15dc975b5ddfc
piv <pivwork001@mail.ru>
2016-12-17 15:02:40 +0300

    botid from file

--- bot/piv_telbot_lp3.py
+++ bot/piv_telbot_lp3.py
@@ -1,7 +1,7 @@
 '''Простой бот для telegram написан в рамках прохождения курся learn python 3.'''
 #import python_telegram_bot
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-from telegram import ReplyKeyboardMarkup,ReplyKeyboardHide
+from telegram import ReplyKeyboardMarkup, ReplyKeyboardHide
 #мои модули
 from piv_getans import get_ans, rand_positive
 from piv_calc import calc_from_words_message, replace_eq
@@ -12,7 +12,7 @@ from piv_wordcount import word_counter
 ###########################################################
 def reset_chat(bot, update, chat_data, msg_text):
     '''Сброс режима.'''
-    print('reset_chat')
+    print('reset_chat msg_text',msg_text)
     chat_data['cmd'] = ''
     chat_data['expression'] = ''
     chat_data['cities'] = {}
@@ -20,10 +20,12 @@ def reset_chat(bot, update, chat_data, msg_text):
     print(hide_keyboard)
     bot.sendMessage(update.message.chat_id, text=msg_text, reply_markup=hide_keyboard) 
 
+
 def text_calc(args_as_str, bot, chat_data, chat_id):
     '''Обработка сообщения для калькулятора.'''
     args_as_str = args_as_str.lower()
     args_as_str = replace_eq(args_as_str)
+    print('text_calc', args_as_str)
     if '=' in args_as_str:
         bot.sendMessage(chat_id, text=calc_from_words_message(args_as_str))
     else:
@@ -65,9 +67,9 @@ def cmd_cities(bot, update, args, chat_data):
     print(type(bot))
     print(type(update))
     print(update.message)
-    reset_chat(bot, update, chat_data, 'Играем в города, начинай!')
     chat_data['cmd'] = 'cities'
     chat_data['cities'] = {}#TODO сюда надо класть список городов
+    reset_chat(bot, update, chat_data, 'Играем в города, начинай!')
 
 
 def cmd_calc(bot, update, args, chat_data):
@@ -75,12 +77,12 @@ def cmd_calc(bot, update, args, chat_data):
     Обработка команды /calc
     Переход в режим калькулятора и вычисление выражения если оно есть
     '''
-    print('Вызван /calc')
-    print(type(bot))
-    print(type(update))
-    print(update.message)
-    print(chat_data)
-    print(args)
+    print('Вызван /calc args', args)
+    #print(type(bot))
+    #print(type(update))
+    #print(update.message)
+    #print(chat_data)
+    #print(args)
     cmd = chat_data.get('cmd', '')
     print('cmd: ', cmd)
     if cmd != 'calc':
@@ -124,7 +126,10 @@ def cmd_start(bot, update, chat_data):
 
 def main():
     '''Главная функция.'''
-    updater = Updater('312255597:AAHeQ-xtkTqSUMpNW08TXhFPLeaQtQGE5D4')
+    botid = ''
+    with open('botid.txt', 'r', encoding='utf-8') as f:
+        botid = f.read()  
+    updater = Updater(botid)
     updater.dispatcher.add_handler(CommandHandler('start',        cmd_start,                      pass_chat_data=True))
     updater.dispatcher.add_handler(CommandHandler('wordcount',    cmd_word_count, pass_args=True, pass_chat_data=True))
     updater.dispatcher.add_handler(CommandHandler('calc',         cmd_calc,       pass_args=True, pass_chat_data=True))
